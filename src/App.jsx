c2bba2c4e1ef11c9520c74479e1dc77bc737759e
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-09-18 23:42:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div>
+          <h1>Do zrobienia</h1>
+          <h2>5 zadan</h2>
+        </div>
+        <button className={styles.button}>+</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;

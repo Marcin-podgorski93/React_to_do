f111a93cd076ad7fb2fada7b00bfec15cbfb4e89
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-09-30 23:40:43 +0200

    create onClick

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -2,12 +2,16 @@ import { Button } from "../Button/Button";
 import styles from "./TodoItem.module.css";
 
 export function TodoItem({ name, done }) {
+  function check(e) {
+    console.log(e);
+  }
+
   return (
     <li className={styles.item}>
       <span className={`${styles.name} ${done ? styles.done : ""}`}>
         {name}
       </span>
-      {!done && <Button>Zrobione</Button>}
+      {!done && <Button onClick={check}>Zrobione</Button>}
       <Button>Usuń</Button>
     </li>
   );
